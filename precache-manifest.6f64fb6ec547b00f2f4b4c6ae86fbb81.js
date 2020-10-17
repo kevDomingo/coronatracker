6b2fe68a48078abc64294a33385ceddb6f82be1b
Kevin Domingo <kevdomingo10@gmail.com>
2020-10-17 01:03:15 -0700

    Updates

---
+++ precache-manifest.6f64fb6ec547b00f2f4b4c6ae86fbb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40793f7aacac66c159c7c9069be0421",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "456b00221772575721e6",
+    "url": "/covidtracker/static/css/main.cd1985e2.chunk.css"
+  },
+  {
+    "revision": "7adfc20ab1c10afb42e1",
+    "url": "/covidtracker/static/js/2.3bc0e6b6.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidtracker/static/js/2.3bc0e6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456b00221772575721e6",
+    "url": "/covidtracker/static/js/main.52da7e9f.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/covidtrackerImage.d7265326.png"
+  }
+]);
