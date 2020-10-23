971819d215c43d262eda87af7f518d10db1194e6
Kevin Domingo <kevdomingo10@gmail.com>
2020-10-23 01:00:29 -0700

    Add files via upload

---
+++ build/precache-manifest.0b850d12b6bfd09fc198fa72863672a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70b4ecfcae178089a31a9d6fb3c6ab9",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "ea1eba894252b6117f32",
+    "url": "/covidtracker/static/css/main.cd1985e2.chunk.css"
+  },
+  {
+    "revision": "7adfc20ab1c10afb42e1",
+    "url": "/covidtracker/static/js/2.3bc0e6b6.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidtracker/static/js/2.3bc0e6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1eba894252b6117f32",
+    "url": "/covidtracker/static/js/main.eb748e43.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/covidtrackerImage.d7265326.png"
+  }
+]);
